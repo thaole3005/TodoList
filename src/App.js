0c54b0b5ba0decc0585dd1e8d8d94411fda4c6b6
LeThao <thaothao30052000@gmail.com>
2021-10-31 01:19:12 +0700

    add TodoList project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { HomeTemplate } from './templates/HomeTemplate/HomeTemplate';
+import Home from './components/Home';
+import TodoListRCC from './TodoList/TodoListRCC.js';
+import TodoListRFC from './TodoList/TodoListRFC';
+import TodoListThunk from './TodoList/TodoListThunk';
+import TodoListSaga from './TodoList/TodoListSaga';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        
+          <HomeTemplate exact path = "/" Component={Home}/>
+          <HomeTemplate exact path = "/home" Component={Home}/>
+          <HomeTemplate exact path = "/todolistRCC" Component={TodoListRCC}/>
+          <HomeTemplate exact path = "/todolistRFC" Component={TodoListRFC}/>
+          <HomeTemplate exact path = "/todoListThunk" Component={TodoListThunk}/>
+          <HomeTemplate exact path = "/todoListSaga" Component={TodoListSaga}/>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
